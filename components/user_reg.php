7682d825210011af35f55d0e7976450848b6195d
Your Name <you@example.com>
2021-09-22 18:27:15 +0500

    db_1fixed

--- components/user_reg.php
+++ components/user_reg.php
@@ -4,16 +4,16 @@
         $dbUser = 'root';
         $dbPass = '';
         $host = 'localhost';
-        return new PDO("mysql:host=$host; dbName=$dbName;", $dbUser, $dbPass);
+        return new PDO("mysql:host=$host; dbname=$dbName;", $dbUser, $dbPass);
     }
     function userReg($username, $name, $password, $photo){
         $pdo = pdo();
         $query = "INSERT INTO users (username, name, password, photo) VALUES (?,?,?,?)";
         $driver = $pdo->prepare($query);
-        $driver->execute([$username, $name, $password, $photo]);
+        $result = $driver->execute([$username, $name, $password, $photo]);
         if ($driver->errorInfo()[0] == '00000') {
             var_dump($driver->errorInfo());
         }
     }
-    userReg('uzcoin404', 'Suyunbek', '1234', 'img');
+    userReg('uzcoin404rewrr', 'Sewruyunbek', '1234', 'trytrew');
 ?>
