f134818c9c1860cea9e6cd344bc99e356d1cf65a
Suyunbek <uzcointg@gmail.com>
2021-09-27 10:51:01 -0700

    reg release

--- components/user_reg.php
+++ components/user_reg.php
@@ -3,9 +3,8 @@
     $login = $_POST['login'];
     $name = $_POST['name'];
     $pass = $_POST['pass'];
-    $imgPath = "../img/avatar/$login.$ext";
     $ext = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
-    move_uploaded_file($_FILES['photo']['tmp_name'], "../img/avatar/$login.$ext");
+    $imgPath = "../img/avatar/$login.$ext";
+    move_uploaded_file($_FILES['photo']['tmp_name'], $imgPath);
     userReg($login, $name, $pass, $imgPath);
-    echo '<h1>Welcome '. $name . ' </h1>', '<p>Your Username '. $login . ' </p>', '<p>Your password '. $pass . ' </p>';
 ?>
